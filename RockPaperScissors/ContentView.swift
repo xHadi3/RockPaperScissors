0ef72f755ff5f12460fc56f08c5e6c7ee21ca045
xHadi3 <hadiessam9@gmail.com>
2024-12-23 15:27:43 +0300

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Hadi Al zayer on 22/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Hadi Al zayer on 22/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
